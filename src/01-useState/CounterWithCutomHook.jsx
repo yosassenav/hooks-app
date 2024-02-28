8852591105b1884eb78e624f3e03527f013a6ffb
yosassenav <mromerovanessa.soy@gmail.com>
2024-02-28 00:20:51 -0500

    Learning useState hook in depth + learning about custom hooks

---
+++ src/01-useState/CounterWithCutomHook.jsx
@@ -0,0 +1,19 @@
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterWithCustomHook = () => {
+
+    const { counter } = useCounter();
+
+    return (
+        <>
+            <h1>Counter with  Custom Hook: {counter}</h1>
+            <hr/>
+
+            <button className="btn btn-primary">+1</button>
+            <button className="btn btn-primary">Reset</button>
+            <button className="btn btn-primary">-1</button>
+
+
+        </>
+    )
+}
