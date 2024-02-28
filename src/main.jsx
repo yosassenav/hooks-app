cd7831b442ab6da0a721834632637b313dac2d6d
yosassenav <mromerovanessa.soy@gmail.com>
2024-02-28 00:21:27 -0500

    adding a custom hook useCounter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp'
-import './index.css'
+import './index.css';
+import { CounterWithCustomHook } from './01-useState/CounterWithCutomHook';
+// import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp/>
+    {/* <HooksApp/> */}
+    {/* <CounterApp/> */}
+    <CounterWithCustomHook/>
   </React.StrictMode>,
 )
