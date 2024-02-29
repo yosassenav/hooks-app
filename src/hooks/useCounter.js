a713e84416c402223b6af81844bdeaae88e3122b
yosassenav <mromerovanessa.soy@gmail.com>
2024-02-29 07:50:32 -0500

    adding functions to custom hook useCounter and adding changes to hook CounterWithCustomHook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,9 +1,29 @@
 import { useState } from "react"
 
-export const usecounterer = (initialValue = 10) => {
+export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue);
+
+    const increment = (value = 1) => {
+        console.log(value); 
+        /**el parametro "value" es un evento y los eventos en JS son objetos,
+         * por lo que por eso marca error al sumar un valor (counter) con un objeto.
+         */
+        setCounter(counter + value);
+    }
+
+    const decrement = (value = 1) => {
+        if (counter === 0) return;
+        setCounter(counter - value);
+    }
+
+    const reset = () => {
+        setCounter(initialValue);
+    }
 
     return {
         counter,
+        increment,
+        decrement,
+        reset
     }
 }
