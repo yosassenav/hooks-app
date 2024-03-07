b685080c77640161cb4c2cec1bb362e6eadecf8c
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-06 23:19:01 -0500

    adding improvements to custom hook useFetch and also to the MultipleCustomHook component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,15 @@
 import { useFetch } from "../hooks/useFetch"
 
 export const MultipeCustomHooks = ()=>{
-    useFetch();
-    return <div>Multiple Custom Hook</div>
+    const {data, isLoading, hasError} = useFetch('https://pokeapi.co/api/v2/pokemon/1');
+    return (
+    <>
+        <h1>Informacion de Pokemon</h1>
+        <hr/>
+        {isLoading && <p>Cargando...</p>}
+
+        {/* <pre>{JSON.stringify(data, null , 2)}</pre> */}
+        <h2>{data?.name}</h2>
+    </>
+    )
 }
