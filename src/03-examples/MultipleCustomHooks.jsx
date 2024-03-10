ed9d713f58b5a6ba08290bf7b2c809794e2c6a5f
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-10 00:24:26 -0500

    section 9 - class 128 integrating both custom hooks: useCounter and useFetch in MultiCustomHook component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,49 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
+import { LoadingMessage } from "./LoadingMessag";
+import { PokemonCard } from "./PokemonCad";
 
 export const MultipeCustomHooks = ()=>{
-    const {data, isLoading, hasError} = useFetch('https://pokeapi.co/api/v2/pokemon/1');
+
+    const {counter, decrement, increment} = useCounter(1);
+    const {data, isLoading} = useFetch(`https://pokeapi.co/api/v2/pokemon/1${counter}`);
+
+    // console.log("data: ",data);
+
     return (
     <>
         <h1>Informacion de Pokemon</h1>
         <hr/>
-        {isLoading && <p>Cargando...</p>}
+        {isLoading
+         ? <LoadingMessage/>
+         : (
+         <PokemonCard
+         id={counter}
+         name={data.name}
+         sprites={[
+            data.sprites.front_default,
+            data.sprites.front_shiny,
+            data.sprites.back_default,
+            data.sprites.back_shiny
+        ]}
+         />
+         )
+        }
 
         {/* <pre>{JSON.stringify(data, null , 2)}</pre> */}
         <h2>{data?.name}</h2>
+        <button 
+        className="btn btn-primary mt-2"
+        onClick={() => counter > 1 ? decrement() : null }
+        >
+        Anterior
+        </button>
+        <button 
+        className="btn btn-primary mt-2"
+        onClick={() => increment()}
+        >
+        Siguiente
+        </button>
     </>
     )
 }
---
+++ src/03-examples/PokemonCad.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export const PokemonCard = ({id, name, sprites=[]}) => {
+    return (
+        <section style={{height: 200}}>
+        <h2 className="text-capitalize">#{id} - {name}</h2>
+
+        {/**Images */}
+        <div>
+            {
+                sprites.map( sprite => (
+                    <img key={sprite} src={sprite} alt={name}/>
+                ))
+            }
+        </div>
+
+        </section>
+    )
+}
+
+PokemonCard.propTypes = {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    sprites: PropTypes.arrayOf(PropTypes.string)
+  };
