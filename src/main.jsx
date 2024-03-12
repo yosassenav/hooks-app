7075f107db2f99abf74a53363ed908272c3de232
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-11 23:58:51 -0500

    a first review on useLayoutEffect hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
+// import { FocusScreen } from './04-useRef/FocusScreen';
 // import { MultipeCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -17,7 +18,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <SimpleForm/> */}
     {/* <FormWithCustomHook/> */}
     {/* <MultipeCustomHooks/> */}
-    <FocusScreen/>
+    {/* <FocusScreen/> */}
+    <Layout/>
   </React.StrictMode>,
 )
 
