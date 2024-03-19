b10e8443b3a553751d4731f7e2bfc057c02e4e5d
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-19 00:37:22 -0500

    using memo function and useCallback hook in example scenario

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
+// import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
 // import { Layout } from './05-useLayoutEffect/Layout';
@@ -15,7 +16,7 @@ import { CallbackHook } from './06-memos/CallbackHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   </React.StrictMode>,
 )
 
