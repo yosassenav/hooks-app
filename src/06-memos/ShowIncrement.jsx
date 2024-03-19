46b4dfc97a96967d941f1b203092a02c1e5b82ee
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-19 00:14:21 -0500

    reviewing useCallback hook with example

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const ShowIncrement = React.memo(({increment}) => {
+    console.log('me vovli a dibujar :c')
+    return (
+    <button
+    className="btn btn-primary"
+    onClick={()=>{
+        increment(5)
+    }}
+    >
+     Incrementar
+    </button>)
+})
+
+ShowIncrement.propTypes = {
+    increment: PropTypes.func.isRequired
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
+// import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { FocusScreen } from './04-useRef/FocusScreen';
@@ -14,7 +15,7 @@ import { MemoHook } from './06-memos/MemoHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoHook/>
+    <CallbackHook/>
   </React.StrictMode>,
 )
 
