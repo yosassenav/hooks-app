0f44cc3a9d3f6d4e6a2153941521fc811c982dc0
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-21 23:56:29 -0500

    todoApp using useReducer hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Padre } from './07-tarea-memo/Padre';
+import { TodoApp } from './08-useReducer/TodoApp';
+// import { Padre } from './07-tarea-memo/Padre';
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
@@ -13,12 +14,11 @@ import { Padre } from './07-tarea-memo/Padre';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCutomHook';
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp';
+// import './08-useReducer/intro-reducer'
 
-import './08-useReducer/intro-reducer'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Padre/>
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TodoApp/>
+  </React.StrictMode>,
+)
 
