2268d389cf6126964cbb78f8b4e07608eabf9556
yosassenav <mromerovanessa.soy@gmail.com>
2024-03-28 00:24:03 -0500

    building a todoApp to understand the useReducer hook

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -18,13 +18,41 @@ export const TodoApp = () =>{
 
     return (
     <>
-        <h1>Todo App</h1>
+        <h1>TodoApp: 10 <small>pendientes: 2</small></h1>
         <hr/>
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+        <div className="row">
+            <div className="col-7">
+                <ul className="list-group">
+                    {
+                        state.map((item) => {
+                            <li key={item.id} className="list-group-item d-flex justify-content-between">
+                                <span className="align-elf-center">Item 1</span>
+                                <button className="btn btn-danger">Borrar</button>
+                            </li>
+                        })
+                    }
+                    
+                    
+                </ul>
+            </div>
+            <div className="col-5">
+                <h4>Agrear To Do</h4>
+                <form>
+                    <input 
+                    type='text'
+                    placeholder="Que hay que hacer?"
+                    className="form-control mb-2"
+                    />
+                    <button
+                    type="submit"
+                    className="btn btn-outline-primary mt-2"
+                    >
+                        Agregar
+                    </button>
+                </form>
+            </div>
+        </div>
+        
     </>
     )
 }
