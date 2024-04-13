1ec8ff46401c6541fb8afd0db7bfe5497dc3b040
yosassenav <mromerovanessa.soy@gmail.com>
2024-04-13 17:30:15 -0500

    refactoring code in custom hook useTodo - end of section 10 useReducer hook

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -6,11 +6,11 @@ import { useTodo } from "./../hooks/useTodo";
 
 export const TodoApp = () => {
 
-    const {todos, handleNewTodo, handleDeleteTodo, handleToggleTodo} = useTodo()
+    const {todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, handleToggleTodo} = useTodo()
 
     return (
     <>
-        <h1>TodoApp: 10 <small>pendientes: 2</small></h1>
+        <h1>TodoApp: {todosCount} <small>pendientes: {pendingTodosCount}</small></h1> 
         <hr/>
         <div className="row">
             <div className="col-7">
