6e233290532cf7b4c5c8f9958b52098e98e82e37
yosassenav <mromerovanessa.soy@gmail.com>
2024-05-05 22:03:19 -0500

    fixing typos in files

---
+++ src/03-examples/LoadingMessage.jsx
@@ -0,0 +1,9 @@
+export const LoadingMessage = () => {
+    return (
+        <section style={{height: 200}}
+        className="alert alert-info text-center p-0 m-0 d-flex align-tems-center justify-contents-center"
+        >
+            <h2>Cargando...</h2>
+        </section>
+    )
+}
