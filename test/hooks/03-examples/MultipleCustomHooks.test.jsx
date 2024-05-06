d0783f618e2d55c41026a8d2238019aac9c809d7
yosassenav <mromerovanessa.soy@gmail.com>
2024-05-05 22:07:43 -0500

    testing the MultipleCustomHooks component

--- test/hooks/03-examples/MultipleCustomHooks.test.jsx
+++ test/hooks/03-examples/MultipleCustomHooks.test.jsx
@@ -1,17 +1,53 @@
 import { render, screen } from "@testing-library/react"
 import { MultipeCustomHooks } from "../../../src/03-examples/MultipleCustomHooks"
+import { useFetch } from "../../../src/hooks/useFetch"
+import "@testing-library/jest-dom";
+
+jest.mock("../../../src/hooks/useFetch");
+
 
 describe('Pruebas en MultipleCustomHooks', () => {
-    test('debe mostrar el componente por defecto',()=>{
-        render( <MultipeCustomHooks/> );
-        expect(screen.getByText('Cargando...'))
-        expect(screen.getByText('Informacion de Pokemon'))
+    useFetch.mockReturnValue({
+         data: null,
+        isLoading: true,
+        hasError: null
+    })
 
-        const nextButton = expect(screen.getByRole('button',{name: 'Siguiente'}));
+    test('debe mostrar el componente por defecto', () => {
+        // Mock the useFetch hook return value
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        });
 
-        expect(nextButton.disabled).toBeTruthy()
+        render(<MultipeCustomHooks />);
+        
+        expect(screen.getByText('Cargando...')).toBeInTheDocument();
+        expect(screen.getByText('Informacion de Pokemon')).toBeInTheDocument();
 
-        screen.debug()
-    }) 
+        const nextButton = screen.getByRole('button', { name: 'Siguiente' });
+        expect(nextButton).toBeDisabled(); // Use toBeDisabled() instead of checking .disabled attribute
+    });
+
+    test('Debe mostrar info de la API',()=>{
+
+       // Mock the useFetch hook return value
+       useFetch.mockReturnValue({
+        data: { name: 'metapod', order: 15, sprites: { 
+            front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+            front_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+            back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+            back_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png"
+        
+        }},
+        isLoading: false,
+        hasError: null
+    });
+
+    render(<MultipeCustomHooks />);
+
+    expect(screen.getByText('metapod')).toBeInTheDocument();
+    })
 
 })
