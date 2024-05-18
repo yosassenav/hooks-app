9e28f2b24f06516b18eab65761012c7477c79507
yosassenav <mromerovanessa.soy@gmail.com>
2024-05-18 16:33:09 -0500

    adding tests for TodoItem component when it is pending to be completed

---
+++ test/hooks/08-useReducer/TodoItem.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { TodoItem } from "../../../src/08-useReducer/TodoItem";
+
+describe('Puebas en >TodoItem/>',()=>{
+
+    const todo = {
+        id: 1,
+        description: 'Aprender JEST',
+        donde: false
+    }
+    const onDeleteTodoMock = jest.fn();
+    const onToggleTodoMock = jest.fn();
+
+    test('Debe mostrar el Todo pendiente por completar',()=>{
+        render(
+        <TodoItem todo={todo}
+        onDeleteTodo={onDeleteTodoMock}
+        onToggleTodo={onToggleTodoMock}/>
+    );
+
+    const liElement = screen.getByRole('listitem');
+    // console.log(liElement.innerHTML);
+    expect(liElement.className).toBe('list-group-item d-flex justify-content-between');
+
+    const spanElement = screen.getByLabelText('span');
+    expect(spanElement.className).toContain('align-self-center');
+    expect(spanElement.className).not.toContain('text-decoration-line-through');
+
+    })
+    test('',()=>{
+
+    })
+    test('',()=>{
+
+    })
+
+})
